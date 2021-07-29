31e3b6ca95aa7f4757dcb943a92aede22a799a1a
hyokyun <gyrbs22@gmail.com>
2021-07-29 23:58:35 +0900

    sixth commit, async by constructor

---
+++ asynchronized/computedProperty.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class Sample {
+  //Computed Property를 통해
+  //생성자에 접근해도, 호출된 시점에서 생성자를 정의하여 비동기적으로 사용 가능
+  *[Symbol.iterator]() {
+    let cnt = 0;
+    cnt = cnt + 1;
+    yield cnt;
+  }
+}
+
+const fn = new Sample();
+const gn = fn[Symbol.iterator]();
+
+console.log(gn.next());
+console.log(gn.next());
+console.log(gn.next());
+
+console.log(Array.from(fn));
+console.log(Array.from(fn));
+console.log(Array.from(fn));
---
+++ asynchronized/generator.js
@@ -0,0 +1,26 @@
+//class 내부의 constructor(생성자)는 비동기함수호출/callback/async사용 모두 불가
+//기본적으로 생성자는 최초 1번만 호출되어 비동기함수호출이 불가.
+
+"use strict";
+
+class Sample {
+  *generate() {
+    let cnt = 0;
+    //++cnt 해당 라인 실행전에 cnt = cnt + 1
+    //cnt++ 해당 라인 실행후에 cnt = cnt + 1
+    cnt = cnt + 1;
+    yield cnt;
+  }
+}
+
+//generate에 접근할 수 있도록 생성자를 통해 접근필요
+//console.log(generate.next());
+
+//아래 결과를 보면 생성자는 최초 1번만 호출되어 비동기적으로 사용할 수 없음을 알 수 있음.
+
+const fn = new Sample();
+const gn = fn.generate();
+
+console.log(gn.next());
+console.log(gn.next());
+console.log(gn.next());

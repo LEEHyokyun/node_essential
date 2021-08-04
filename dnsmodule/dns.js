a6f5987d1c1811e234f082f473f98c48451cc3c6
hyokyun <gyrbs22@gmail.com>
2021-08-04 20:27:43 +0900

    dns.js added commit

--- dnsmodule/dns.js
+++ dnsmodule/dns.js
@@ -18,6 +18,7 @@ dns.resolve4("google.com", (err, addresses) => {
     addresses.forEach((a) => {
       dns.reverse(a, (err, hostnames) => {
         if (err) throw err;
+        console.log(hostnames);
         console.log(`reverse for ${a}, ${JSON.stringify(hostnames)}`);
       });
     });

b0ecd6ada83427483f6d121a28604275f18796b8
hyokyun <gyrbs22@gmail.com>
2021-08-04 20:25:34 +0900

    added commit, dns script modified

--- dnsmodule/dns.js
+++ dnsmodule/dns.js
@@ -1,26 +1,26 @@
-'use strict'
+"use strict";
 
-const dns = require('dns')
+const dns = require("dns");
 
 //dns 문자주소를 탐색
-dns.lookup('google.com')(err, address, family) =>{
-  console.log(`address :${address}, ${family}`)
-}
+dns.lookup("google.com", (err, address, family) => {
+  console.log(`address :${address}, ${family}`);
+});
 
-dns.resolve4('google.com', (err, addresses) => {
-  if(err) throw err
-}
+dns.resolve4("google.com", (err, addresses) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const res = JSON.stringify(addresses);
 
-const res = JOSN.stringify(addresses)
+    console.log(res);
 
-console.log(res)
-
-addresses.forEach( a => {
-  dns.reverse(a, (err, hostnames) => {
-    if(err) throw err
-    console.log(`reverse for ${a}, ${JSON.stringify(hostnames)}`)
-  })
-})
-  
-//reverse for IP address, www.google.com
-         
+    addresses.forEach((a) => {
+      dns.reverse(a, (err, hostnames) => {
+        if (err) throw err;
+        console.log(`reverse for ${a}, ${JSON.stringify(hostnames)}`);
+      });
+    });
+  }
+  //reverse for IP address, www.google.com
+});

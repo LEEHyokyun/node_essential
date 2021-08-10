4dc04ca9b4ba28cdd6a179fb6d89c42a0033ed2e
hyokyun <gyrbs22@gmail.com>
2021-08-10 13:03:05 +0900

    styled-component : initial setting

---
+++ styled-component/styled/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+
+import logo from './logo.svg';
+//import './App.css';
+
+const Component = styled.div`
+  color : white;
+`
+
+function App() {
+  return (
+    <Component title = "this is styled"/>
+  )
+}
+
+export default App;

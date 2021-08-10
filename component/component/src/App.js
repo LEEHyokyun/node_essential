936574ffa5ae91a08e13d3a4ed35eef0eb1e74e1
hyokyun <gyrbs22@gmail.com>
2021-08-10 10:50:14 +0900

    Component final commit : adapting component refactoring_refactoring step_2

--- component/component/src/App.js
+++ component/component/src/App.js
@@ -2,36 +2,27 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Comment = (props) => {
-  <div className="comment">{props.children}</div>
-} //component화 이후 이 자체로 사용가능
-
-const UserInfo = (props) => {
-  <div className="Avatar">{props.children}</div>
-}
-
-//Comment와 UserInfo에서, 벌써부터 중복되는 부분이 나타나기 시작했다.
-//중복되는 부분을 제거하는 작업이 필요하다.
+const Component = (props) => <div className={props.name}>{props.children}</div>
 
 function App() {
   return (
-    <Comment> {/*comment를 관리하는 component*/}
-      <div className="UserInfo"> {/*comment 작성한 유저정보를 나타내는 component*/}
+    <Component name="comment"> {/*comment를 관리하는 component*/}
+      <Component Name = "UserInfo"> {/*comment 작성한 유저정보를 나타내는 component*/}
         <img className="Avatar" //해당 사용자의 이미지를 나타내는 component
           src={props.author.avatarUrl}
           alt={props.author.name}
         />
-        <div className="UserInfo-name"> {/*유저이름을 나타내는 component*/}
+        <Component name="UserInfoName"> {/*유저이름을 나타내는 component*/}
           {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">
+        </Component>
+      </Component>
+      <Component name="Comment-text">
         {props.text}
-      </div>
-      <div className="Comment-date">
+      </Component>
+      <Component name="Comment-date">
         {formatDate(props.date)}
-      </div>
-    </Comment>
+      </Component>
+    </Component>
     )
 }
 export default App;

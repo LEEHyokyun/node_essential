d3a0976b1fb3cec26e141adacb040bd82b0e982e
hyokyun <gyrbs22@gmail.com>
2021-08-11 23:52:00 +0900

    redis first set commit

---
+++ toy_redis/index.js
@@ -0,0 +1,20 @@
+const redis = require('redis')
+const client = redis.createClient()
+
+//client가 redis에 connect event 발생시 log출력
+client.on('connect', ()=>{
+    console.log('connected')
+})
+
+//redis store에 key값에 대한 value값 매핑하여 설정(저장)
+client.set('backend', 'node', (err, res) =>{
+    console.log(res)
+})
+
+//redis store에 key값에 대한 value값을 확보한다
+client.get('backend', (err,res) => {
+    console.log(res)
+})
+
+//redis에 저장된 key value값을 특정 시간 이후에 제거
+client.expire('backend', 20)

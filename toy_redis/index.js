e782fed45fdc6f819b096f15d195e8e2555e76d3
hyokyun <gyrbs22@gmail.com>
2021-08-12 10:59:50 +0900

    redis data type commit(base structure)

--- toy_redis/index.js
+++ toy_redis/index.js
@@ -18,3 +18,30 @@ client.get('backend', (err,res) => {
 
 //redis에 저장된 key value값을 특정 시간 이후에 제거
 client.expire('backend', 20)
+
+//data delete
+client.del('backend', (err, res) => {
+    if(err){
+        console.err(err)
+    }
+    console.log(res)
+})
+
+//data update
+client.set('key', 1, ()=>{
+    client.incr('key', (err, res)=> {
+        if(err){
+            console.error(err)
+        }
+        console.log(res)
+    })
+})
+
+//data update
+client.set('value2', 5, ()=>{
+    client.decr('value2', (err, res) => {
+        if(err) { console.error(err)
+        }
+        console.log(res)
+    })
+})

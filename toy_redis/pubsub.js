33f7a1e33b6059c0fed8a59a1185174a617aee85
hyokyun <gyrbs22@gmail.com>
2021-08-12 12:49:31 +0900

    pub / sub structrue set commit

---
+++ toy_redis/pubsub.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const redis = require('redis')
+
+const publisher = redis.createClient()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const data = {  //Routing
+        key : 'string_value' 
+    }
+    //publisher에서 creatClient를 통해 server 접근
+    //이벤트정의(notify)
+    //이벤트유도 후 data를 stringify하여 전달
+    publisher.publisher("notify", JSON.stringify(data))
+
+    //response
+    res.send("Pub sent event")
+})
+
+//port binding
+app.listen(8000, ()=> {
+    console.log('Running at PORT 8000')
+})
